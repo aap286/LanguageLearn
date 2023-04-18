182ddf7e24b17b83052fd076165b85a5554ea673
aap286 <aryan.apatil@gmail.com>
2023-04-18 11:42:15 -0400

    using vite

---
+++ client/App.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [backendData, setBackendDData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendDData(data);
+      });
+  }, []);
+  return (
+    <section>
+      {typeof backendData.users === "undefined" ? (
+        <p>loading...</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+    </section>
+  );
+}
+
+export default App;
