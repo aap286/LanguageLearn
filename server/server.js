6a8866949963d743317b13dad76ec27f2f95f74a
aap286 <aryan.apatil@gmail.com>
2023-04-18 19:10:55 -0400

    learning one

--- server/server.js
+++ server/server.js
@@ -5,4 +5,5 @@ app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo"]});
 })
 
+// starts server at 5000 port
 app.listen(5000, () => {console.log("Server started at port 5000") })
