a61683e8952bbd93bea6aba8fea0f23184f27410
aap286 <aryan.apatil@gmail.com>
2023-04-18 19:36:02 -0400

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
-import React, {useEffect, useState} from 'react'
+// ! to start server in server directory npm start
+
+import React, { useEffect, useState } from "react";
+import Helloworld from "./scripts/Helloworld";
 
 function App() {
+  const [backendData, setBackendData] = useState([{}]);
 
-  const [backendData, setBackendData] = useState([{}])
-
-  useEffect(() => { 
+  useEffect(() => {
     fetch("/api")
-      .then(response => response.json())
-      .then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  })
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  });
 
   return (
-    <div>
-      {typeof backendData.users === "undefined" ? (
-        <p>loading...</p>
-      ) : (
-        backendData.users.map((user, i) => <p key={i}>{user}</p>)
-      )}
-    </div>
+    <>
+      <div>
+        {typeof backendData.users === "undefined" ? (
+          <p>loading...</p>
+        ) : (
+          backendData.users.map((user, i) => <h1 key={i}>{user}</h1>)
+        )}
+      </div>
+
+      <div>
+        <Helloworld />
+        <Helloworld />
+      </div>
+    </>
   );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get("/api", (req, res) => { 
-    res.json({"users": ["userOne", "userTwo"]});
-})
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo"] });
+});
 
-// starts server at 5000 port
-app.listen(5000, () => {console.log("Server started at port 5000") })
+// starts server at 5000
+app.listen(5000, () => {
+  console.log("Server started at port 5000");
+});
